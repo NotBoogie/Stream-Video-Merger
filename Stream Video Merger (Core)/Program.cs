406ddc96627507a3b48562bab65784a995b2251e
NotBoogie <86927425+NotBoogie@users.noreply.github.com>
2022-08-01 09:00:45 -0400

    Updating error checks + adding settings options

--- Stream Video Merger (Core)/Program.cs
+++ Stream Video Merger (Core)/Program.cs
@@ -8,6 +8,7 @@ using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Stream_Video_Merger__Core_.Settings;
 
 namespace Stream_Video_Merger__Core_
 {
@@ -16,69 +17,87 @@ namespace Stream_Video_Merger__Core_
         //This just takes in stream recordings and kicks out one big video
         static void Main(string[] args)
         {
-            if (args.Length == 0)
+            try
             {
-                Console.WriteLine("No input files; drag all stream files into the exe to merge into one long recording. Videos will be merged in alphabetically");
+                List<string> allVideos = new List<string>();
+                List<string> allMusic = new List<string>();
+                Settings.LoadSettings("StreamVideoMerger");
+
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("No input files; drag all stream files into the exe to merge into one long recording. Videos will be merged in alphabetically");
+                    Console.ReadKey();
+                    return;
+                }
+
+                Console.WriteLine("Enter result filename [result will be x.mp4]: ");
+                string resultName = Console.ReadLine();
+
+                String resultVideo = resultName + "." + Settings.ResultFileExtension;
+
+                string[] audioFileTypes = { ".MP3", ".WAV", ".OGG", ".WMA" };    //Check if ffmpeg can even use crap like .wma
+                                                                                 //Assuming this shit will always come in alp
+                foreach (var item in args)
+                {
+                    var mediaInfoo = FFProbe.Analyse(item);
+                    if (!(audioFileTypes.Contains(Path.GetExtension(item).ToUpper()))) //Is there a non-shit way to do this?
+                        allVideos.Add(item);
+                    else
+                        allMusic.Add(item);
+                }
+
+                allVideos.Sort();   //Alphabetically because why would you want a later part of the piece in the front?
+
+                Console.WriteLine("Merging:");
+                foreach (var item in allVideos)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+
+                try
+                {
+                    FFMpeg.Join(resultVideo,
+                       allVideos.ToArray()
+                    );
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error encountered when attempting to merge. This is most likely because you don't have FFMpeg.exe and FFProbe.exe in your PATH");
+                    Console.WriteLine("Actual error trace: " + e.Message);
+                    Console.ReadKey();
+                    return;
+                }
+
+                FileInfo f = new FileInfo(resultVideo);
+                string fullname = f.FullName;
+
+                Console.WriteLine("Result: " + fullname);
+                if (Settings.MakeZip)
+                {
+                    Console.WriteLine("Making zip version");
+                    makeZip(resultName, fullname);
+                    Console.WriteLine("Zip version created");
+                }
+
+                if (Settings.MakeWebm)
+                {
+                    Console.WriteLine("Generating webm version (NOTE: this will take a while for large files!)");
+                    FFMpegArguments
+                    .FromFileInput(resultVideo)
+                    .OutputToFile(resultVideo.Replace("mp4", "webm"), true)
+                    .ProcessSynchronously();
+
+                }
+                Console.WriteLine("Done");
+                Console.ReadKey();
+                return;
+            }catch(Exception e)
+            {
+                Console.WriteLine("Misc error encountered");
+                Console.WriteLine("Actual error trace: " + e.Message);
                 Console.ReadKey();
                 return;
             }
-
-            Console.WriteLine("Enter result filename [result will be x.mp4]: ");
-            string resultName = Console.ReadLine();
-
-            string workingFormat = "mp4";
-
-            String resultVideo = resultName + "." + workingFormat;
-
-            List<string> allVideos = new List<string>();
-            List<string> allMusic = new List<string>();
-
-            string[] audioFileTypes = { ".MP3", ".WAV", ".OGG", ".WMA" };    //Check if ffmpeg can even use crap like .wma
-            //Assuming this shit will always come in alp
-            foreach (var item in args)
-            {
-                var mediaInfoo = FFProbe.Analyse(item);
-                if (!(audioFileTypes.Contains(Path.GetExtension(item).ToUpper()))) //Is there a non-shit way to do this?
-                    allVideos.Add(item);
-                else
-                    allMusic.Add(item);
-            }
-
-            allVideos.Sort();   //Alphabetically because why would you want a later part of the piece in the front?
-
-            Console.WriteLine("Merging");
-            foreach (var item in allVideos)
-            {
-                Console.WriteLine(item.ToString());
-            }
-
-            FFMpeg.Join(resultVideo,
-               allVideos.ToArray()
-            );
-
-            FileInfo f = new FileInfo(resultVideo);
-            string fullname = f.FullName;
-
-            Console.WriteLine("Result: " + fullname);
-
-            Console.WriteLine("Making zip version");
-            makeZip(resultName, fullname);
-            Console.WriteLine("Zip version created");
-
-            Console.WriteLine("Generate webm version too? (This will take like 5 years to finish for long videos) [Y/N]: ");
-            string makeWebm = Console.ReadLine();
-            if (makeWebm.Trim().ToUpper() == "Y")
-            {
-                Console.WriteLine("Generating webm version");
-                FFMpegArguments
-                .FromFileInput(resultVideo)
-                .OutputToFile(resultVideo.Replace("mp4", "webm"), true)
-                .ProcessSynchronously();
-
-            }
-            Console.WriteLine("Done");
-            Console.ReadKey();
-            return;
         }
         static string makeZip(string filename, string mergedFileLocation)
         {
---
+++ Stream Video Merger (Core)/Settings.cs
@@ -0,0 +1,108 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stream_Video_Merger__Core_
+{
+
+    /// <summary>
+    /// Global settings class
+    /// </summary>
+    public static class Settings
+    {
+        public static string FilenameKey { get; set; }
+        public static string SettingsFileLoc { get { return Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName) + @"/" + FilenameKey + "_Settings.json"; } }
+
+        public static bool Loaded { get; set; } = false;
+
+        public static bool MakeZip { get; set; } = true;
+        public static bool MakeWebm { get; set; } = false;
+        public static string ResultFileExtension { get; set; } = "mp4";
+        public static void LoadSettings(string filenameKey)
+        {
+            FilenameKey = filenameKey;
+            Console.WriteLine("Loading settings file from: " + SettingsFileLoc);
+            SettingsTest_B settingsObj = new SettingsTest_B();
+            if (File.Exists(SettingsFileLoc))
+            {
+                using (StreamReader r = new StreamReader(SettingsFileLoc))
+                {
+                    string json = r.ReadToEnd();
+                    settingsObj = JsonConvert.DeserializeObject<SettingsTest_B>(json);
+                }
+                settingsObj.Loaded = true;
+            }
+            else
+            {
+                Console.WriteLine("Settings file not found, creating default...");
+                SaveSettings();
+            }
+            MapToStaticClass(settingsObj);
+        }
+        public static void SaveSettings()
+        {
+            SettingsTest_B settingsObj = MapFromStaticClass();
+            File.WriteAllText(SettingsFileLoc, JsonConvert.SerializeObject(settingsObj));
+        }
+        public static void MapToStaticClass(SettingsTest_B source)
+        {
+            var sourceProperties = source.GetType().GetProperties();
+
+            //Key thing here is to specify we want the static properties only
+            var destinationProperties = typeof(Settings)
+                .GetProperties(BindingFlags.Public | BindingFlags.Static);
+
+            foreach (var prop in sourceProperties)
+            {
+                if (!prop.CanWrite) continue;
+                //Find matching property by name
+                var destinationProp = destinationProperties
+                    .Single(p => p.Name == prop.Name);
+
+                //Set the static property value
+                destinationProp.SetValue(null, prop.GetValue(source));
+            }
+        }
+
+        public static SettingsTest_B MapFromStaticClass()
+        {
+            SettingsTest_B bb = new SettingsTest_B();
+            var destinationProperties = bb.GetType().GetProperties();
+
+            //Key thing here is to specify we want the static properties only
+            var sourceProperties = typeof(Settings)
+                .GetProperties(BindingFlags.Public | BindingFlags.Static);
+
+            foreach (var prop in destinationProperties)
+            {
+                if (!prop.CanWrite) continue;
+                //Find matching property by name
+                var sourceProp = sourceProperties
+                    .Single(p => p.Name == prop.Name);
+
+                //Set the static property value
+                prop.SetValue(bb, sourceProp.GetValue(null));
+            }
+            return bb;
+        }
+    }
+
+
+    /// <summary>
+    /// Fake class which maps to the real static one, has the same properties but not static
+    /// </summary>
+    public class SettingsTest_B
+    {
+        public string FilenameKey { get; set; }
+        public string SettingsFileLoc { get { return Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName) + @"/" + FilenameKey + "_Settings.json"; } }
+        public bool Loaded { get; set; } = false;
+
+        public bool MakeZip { get; set; } = true;
+        public bool MakeWebm { get; set; } = false;
+        public string ResultFileExtension { get; set; } = "mp4";
+    }
+}
